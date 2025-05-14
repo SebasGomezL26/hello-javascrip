54bf887544e6bdc79ee6932370416f1396ae6ac1
SebasG <xsebastianzg@gmail.com>
2025-05-14 11:10:09 -0500

    Corrijo submódulo y agrego archivos de HELLO-JAVASCRIP

---
+++ HELLO-JAVASCRIP/Basic/00-helloworld.js
@@ -0,0 +1,11 @@
+//Esto es un comentario simple
+
+console.log("Hola, javaScrip")
+console.log("Hola, javaScrip")
+console.log("Hola, javaScrip")
+
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
---
+++ HELLO-JAVASCRIP/Basic/02-datatypes.js
@@ -0,0 +1,48 @@
+//Tipos de datos primitivos
+
+// Cadenas de texto (String) 
+let name = " Sebastian Gomez "
+let alias = 'Gomez' 
+let email = 'xsebastianzg@gmail.com'
+
+// Numeros (number)
+let age = 37 // Entero
+let height = 1.45 // Desimal 
+
+// Booleanos (Boolean)
+let isTeacher = true
+let isStudent = false
+
+// Undefained
+let UndefainedValue
+console.log(UndefainedValue)
+
+
+// Null
+let nullValue = null
+
+//Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BigInt
+let myBigInt = BigInt("234235346623121321454675865342323435364")
+let myBigInt2 = 234235346623121321454675865342323435364n
+
+//Mostramos los tipos de datos
+console.log(typeof name)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+
+console.log(typeof UndefainedValue)
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
