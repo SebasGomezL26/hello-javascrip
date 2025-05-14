d8310edcd9ef76091b7852685eeed9c68dea1427
SebasG <xsebastianzg@gmail.com>
2025-05-14 11:39:53 -0500

    solucion de beginer-exercises

---
+++ HELLO-JAVASCRIP/Basic/03-beginer-execises.js
@@ -0,0 +1,30 @@
+// una linea 
+
+/*varias lineas 
+como 
+este*/
+
+//Declara variables con valores asociados a todos los datos primitivos
+
+var cacita ="¡Hola, JavaScrip!"
+console.log(cacita)
+
+saludoJS = "¡Hola de nuevo, JavaScrip!"
+console.log(saludoJS)
+
+//let
+
+let helloWorld2 ="¡Hola, JavaScrip 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScrip 2!"
+console.log(helloWorld2)
+
+// const
+
+const  helloWorld3 ="¡Hola, JavaScrip 3!"
+console.log(helloWorld3)
+
+// imprimir por consola dotos los valores asociados
+
+
